9fd2b52417557e1fd5536c7de22895b8bcb9312a
shawn233 <739368858@qq.com>
2020-02-07 21:03:38 +0800

    solve problem 66 plus one

---
+++ Leetcode/66-Plus_One/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    // 4ms solution 1
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size()-1; i >= 0; -- i) {
+            digits[i]+=1;
+            if (digits[i] == 10)
+                digits[i] = 0;
+            else
+                return digits;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
